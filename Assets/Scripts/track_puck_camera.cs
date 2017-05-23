751d5de7d37ed55ac773190f14282d5b5943ffb9
Woody Hill <wondertrot@gmail.com>
2017-05-23 15:04:33 +1000

    added moving camera and started to edit level

--- Assets/Scripts/track_puck_camera.cs
+++ Assets/Scripts/track_puck_camera.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//FIXME: compress code into one script
-//TODO: look into adding sfx when colliding
-public class track_puck_floor : MonoBehaviour
+public class track_puck_camera : MonoBehaviour
 {
 
 	public SteamVR_TrackedObject controller;
 
 	private bool isActive;
-
 
 	// Use this for initialization
 	void Start ()
@@ -18,12 +15,14 @@ public class track_puck_floor : MonoBehaviour
 		isActive = true;
 
 	}
-
+	
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.position = new Vector3 (controller.transform.position.x, transform.position.y, controller.transform.position.z);
+		
+		transform.position = new Vector3 (controller.transform.position.x, controller.transform.position.y, controller.transform.position.z);
 		transform.rotation = controller.transform.rotation;
+			
 	}
 
 	void OnTriggerEnter (Collider other)
@@ -42,7 +41,6 @@ public class track_puck_floor : MonoBehaviour
 		isActive = false;
 		Renderer rend = GetComponent<Renderer> ();
 		rend.material.SetColor ("_Color", Color.black);
-		//TODO: change cube scale when colliding
 	}
 
 	void Activate ()
@@ -50,6 +48,5 @@ public class track_puck_floor : MonoBehaviour
 		isActive = true;
 		Renderer rend = GetComponent<Renderer> ();
 		rend.material.SetColor ("_Color", Color.green);
-		//TODO: revert cube scale when colliding
 	}
 }
