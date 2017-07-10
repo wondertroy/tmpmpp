8db25d1bc6d525f674b75ff49eec889f76a6d54f
Woody Hill <wondertrot@gmail.com>
2017-07-10 19:40:29 +1000

    added holograms and improved level to be more open and visible

---
+++ Assets/Scripts/restart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restart : MonoBehaviour {
+
+	public GameObject gate1;
+
+	public GameObject gate2;
+
+	public GameObject gate3;
+
+
+		void Update() {
+			if (Input.GetKeyDown ("space"))
+				gate1.SetActive (true);
+				gate2.SetActive (true);
+			    gate3.SetActive (true);
+
+		}
+	}
+
